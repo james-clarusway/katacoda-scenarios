ab957f92b25d707eae951b82ee0531392fa4e09e
aaron <aaron.clarusway.com>
2020-06-27 18:41:07 +0300

    last2

---
+++ linux-basics/courseBase.sh
@@ -0,0 +1,10 @@
+mkdir /root/one
+mkdir /root/two
+mkdir /root/three
+mkdir /root/two/library
+mkdir /root/two/myDirectories
+mkdir /root/two/linux
+mkdir /root/three/document
+mkdir /root/three/music
+
+echo "This is emty" > /root/clarusway
